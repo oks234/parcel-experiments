1d42e53aea8c3414e15247a5e2b16048259c0f2b
oks234 <bestspx@gmail.com>
2020-01-26 18:47:43 +0900

    #4 TypographyJS and Building for Production

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-import styles from './styles.scss';
+import styles from "./styles.scss";
 
 const App = () => {
-  return <div className={styles.title}>This is App</div>;
+  return (
+    <>
+      <div className={styles.title}>This is App</div>
+      <h1>Header 1</h1>
+      <p>Paragraph 1</p>
+    </>
+  );
 };
 
 export default App;
